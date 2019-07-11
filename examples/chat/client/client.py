f98fe103c9c8fe3c7fb159b5ee35b7631b057d18
littlefish12345 <39996700+littlefish12345@users.noreply.github.com>
2019-07-11 16:02:35 +0800

    Update client.py

--- examples/chat/client/client.py
+++ examples/chat/client/client.py
@@ -2,12 +2,12 @@ import os,sys
 sys.path.append(os.getcwd()+"\\..\\..\\..\\api\\TPWL_client_api")
 import TPWL_C
 
-s = TPWL_C.TPWL_C(8192)
+s = TPWL_C.TPWL_C()
 s.create(1024)
 s.connect(("127.0.0.1",5000))
 while True:
     data = input(">>>")
-    s.send(data)
+    s.send(data.encode())
     if data == "quit":
         break
 s.close()
