cb6421893476b8901d83d4c55fb577842cbf020e
littlefish12345 <39996700+littlefish12345@users.noreply.github.com>
2019-07-11 16:04:10 +0800

    Update TPWL_C.py

--- api/TPWL_client_api/TPWL_C.py
+++ api/TPWL_client_api/TPWL_C.py
@@ -1,73 +1,52 @@
 import rsa,time
 from socket import *
 
-def version():
-    return 0.11
+def version(): #打印版本号
+    return 'beta 0.12'
 
-class TPWL_C:
-    def __init__(self,buffsize):
+class TPWL_C: #定义客户端类
+    def __init__(self): #创建客户端
         self.s = socket(AF_INET,SOCK_STREAM)
-        self.buffsize = buffsize
-        return None
 
-    def create(self,bits):
+    def create(self,bits): #创建密钥对
         self.bits = bits
         self.g_key = None
         
-        try:
-            (public_key, private_key) = rsa.newkeys(bits)
-            self.g_key = (public_key, private_key)
-        except:
-                return 1
-        return 0
+        (public_key, private_key) = rsa.newkeys(bits)
+        self.g_key = (public_key, private_key)
+        
+        return 0 #创建成功返回0
 
-    def connect(self,addr):
+    def connect(self,addr): #连接服务端
         self.addr = addr
         self.s_key = None
         
-        try:
-            self.s.connect(self.addr)
-        except:
-            return 1
+        self.s.connect(self.addr) #连接服务端
         
-        self.s.send(str(self.bits).encode())
+        self.s.send(str(self.bits).encode()) #发送密钥位数
         
-        try:
-            self.s.send(self.g_key[0].save_pkcs1())
-        except:
-            return 3
+        self.s.send(self.g_key[0].save_pkcs1()) #发送公钥
             
-        try:
-            self.s_key = rsa.PublicKey.load_pkcs1(self.s.recv(self.buffsize))
-        except:
-            return 2          
-        return 0
-
-    def send(self,data):
-        try:
-            added_data = rsa.encrypt(data.encode(),self.s_key)
-        except:
-            return 1
+        self.s_key = rsa.PublicKey.load_pkcs1(self.s.recv(self.bits)) #接收公钥
         
-        try:
-            self.s.send(added_data)
-        except:
-            return 2
+        return 0 #协议通讯成功返回0
+
+    def send(self,data): #发送数据
         
-        return 0
+        added_data = rsa.encrypt(data,self.s_key) #用接收成功的公钥加密
 
-    def recv(self):
-        try:
-            data = self.s.recv(self.buffsize)
-        except:
-            return 1
+        self.s.send(added_data) #把加密后的数据发送至服务器
         
-        try:
-            decoded = rsa.decrypt(data, self.g_key[1]).decode()
-        except:
-            return 2
+        return 0 #发送成功返回0
 
-        return decoded
+    def recv(self,buffsize): #接收数据
+        
 
-    def close(self):
+        data = self.s.recv(buffsize) #接收来自服务器的数据
+        
+        decoded = rsa.decrypt(data, self.g_key[1]) #解密数据
+
+        return decoded #返回解密后的数据
+
+    def close(self): #关闭socket
         self.s.close()
