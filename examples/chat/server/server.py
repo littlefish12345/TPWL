92af15d2f2d9d9516c393f42b3488009f05c3513
littlefish12345 <39996700+littlefish12345@users.noreply.github.com>
2019-07-11 16:03:00 +0800

    Update server.py

--- examples/chat/server/server.py
+++ examples/chat/server/server.py
@@ -2,12 +2,12 @@ import os,sys
 sys.path.append(os.getcwd()+"\\..\\..\\..\\api\\TPWL_server_api")
 import TPWL_S
 
-s = TPWL_S.TPWL_S(8192)
+s = TPWL_S.TPWL_S()
 s.bind(("127.0.0.1",5000))
 s.listen(3)
 conn,addr = s.accept()
 while True:
-    data = conn.recv()
+    data = conn.recv(8192).decode()
     if data == "quit":
         break
     print(data)
